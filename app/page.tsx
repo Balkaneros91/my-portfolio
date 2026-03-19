7f25cc72ba202f038824be1cfe86e8ec6cd4f34c
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2026-03-19 14:53:06 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className=" flex min-h-screen items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Antonio-Claudio Andelic</h1>
+        <p className="mt-2 text-lg text-zinc-600">Portfolio coming soon</p>
+      </div>
+    </div>
+  );
+}
